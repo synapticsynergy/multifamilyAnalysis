6817db1b2a2c84e124f12d14506e6da92dd5f8b3
Blake Danson <blakedanson@gmail.com>
2018-11-11 09:57:24 -0800

    Add bootsrap 4 styles

--- client/src/Containers/PropertyInput/PropertyInput.js
+++ client/src/Containers/PropertyInput/PropertyInput.js
@@ -1,61 +1,138 @@
 import React from 'react';
+import {
+  Container,
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText
+} from 'reactstrap';
+import './PropertyInput.css';
 
-function PropertyInput(){
-    return (
-        <form>
-            <div>
-                <p>Address</p>
-            </div>
-            <div>
-                <input type="text" />
-            </div>
-            <div>
-                <p>Number of Units</p>
-            </div>
-            <div>
-                <input type="text" />
-            </div>
-            <div>
-                <p>Rentable SQFT</p>
-            </div>
-            <div>
-                <input type="text" />
-            </div>
-            <div>
-                <p>Purchase Price</p>
-            </div>
-            <div>
-                <input type="text" />
-            </div>
-            <div>
-                <p>Loan To Value</p>
-            </div>
-            <div>
-                <input type="text" />
-            </div>
-            <div>
-                <p>Closing Costs %</p>
-            </div>
-            <div>
-                <input type="text" />
-            </div>
-            <div>
-                <p>Monthly Debt Service</p>
-            </div>
-            <div>
-                <input type="text" />
-            </div>
-            <div>
-                <p>Gross Income</p>
-            </div>
-            <div>
-                <input type="text" />
-            </div>
-            <div>
-                <p>Net Operating Income</p>
-            </div>
-      </form>
-    )
+function PropertyInput() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+            <FormGroup>
+              <Row>
+                <Col>
+                  <div>
+                    <p>Address</p>
+                  </div>
+                  <div>
+                    <Input type="text" />
+                  </div>
+                </Col>
+                <Col>
+                  <div>
+                    <p>Number of Units</p>
+                  </div>
+                  <div>
+                    <Input type="text" />
+                  </div>
+                </Col>
+                <Col>
+                  <div>
+                    <p>Rentable SQFT</p>
+                  </div>
+                  <div>
+                    <Input type="text" />
+                  </div>
+                </Col>
+              </Row>
+
+              <Container>
+                <Row>
+                  <Col>
+                    <div>
+                      <p>Address</p>
+                    </div>
+                    <div>
+                      <Input type="text" />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div>
+                      <p>Number of Units</p>
+                    </div>
+                    <div>
+                      <Input type="text" />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div>
+                      <p>Rentable SQFT</p>
+                    </div>
+                    <div>
+                      <Input type="text" />
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <div>
+                      <p>Purchase Price</p>
+                    </div>
+                    <div>
+                      <Input type="text" />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div>
+                      <p>Loan To Value</p>
+                    </div>
+                    <div>
+                      <Input type="text" />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div>
+                      <p>Closing Costs %</p>
+                    </div>
+                    <div>
+                      <Input type="text" />
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <div>
+                      <p>Monthly Debt Service</p>
+                    </div>
+                    <div>
+                      <Input type="text" />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div>
+                      <p>Gross Income</p>
+                    </div>
+                    <div>
+                      <Input type="text" />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div>
+                      <p>Net Operating Income</p>
+                    </div>
+                    <div>
+                      <Input type="text" />
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </FormGroup>
+            <Button>Analyze</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default PropertyInput;
--- client/src/Containers/PropertyInput/PropertyInput.test.js
+++ client/src/Containers/PropertyInput/PropertyInput.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import PropertyInput from './PropertyInput';
 
-describe('PropertyInput Container',()=>{
-    it('should render without crashing',()=>{
-        const div = document.createElement('div');
-        ReactDom.render(<PropertyInput />,div);
-        ReactDom.unmountComponentAtNode(div);
-    });
+describe('PropertyInput Container', () => {
+  it('should render without crashing', () => {
+    const div = document.createElement('div');
+    ReactDom.render(<PropertyInput />, div);
+    ReactDom.unmountComponentAtNode(div);
+  });
 });
