d3834b353ace4fd9ada13655fb8d1bc02c9ff4e5
Blake Danson <blakedanson@gmail.com>
2019-02-07 11:14:38 -0800

    Grid property summary info

--- client/src/Containers/PropertySummary/PropertySummary.js
+++ client/src/Containers/PropertySummary/PropertySummary.js
@@ -1,44 +1,65 @@
 import React from 'react';
 import './PropertySummary.css';
+import {
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
 
 export default function PropertySummary() {
   return (
     <div>
       <div className="summary-section">
+        <div className="summary-section-item" style={{ marginBottom: '15px' }}>
+          <img width="90%" src="https://picsum.photos/1080/720" alt="" />
+        </div>
+      </div>
+      <div className="summary-section">
         <div>
           <h1>PropertySummary</h1>
         </div>
-        <div>
-          <img src="https://picsum.photos/300/200" alt="" />
-        </div>
-        <div>
-          <p>GRM: 10</p>
-          <p>Cost Per Unit: $100,000</p>
-          <p>Cap Rate: 6%</p>
-          <p>Cash on Cash Return: 4%</p>
-          <p>Debt Service Coverage Ratio: 1.2</p>
-        </div>
-        <div>
-          <h4>1 Year Summary</h4>
-          <p>Appreciation</p>
-          <p>Depreciation</p>
-          <p>Amortization</p>
-          <p>Unleveraged IRR: 10%</p>
-          <p>Leveraged IRR: 11%</p>
-        </div>
+        <Container>
+          <Row>
+            <Col xs="12" md="6">
+              <div className="summary-section-item">
+                <p>GRM: 10</p>
+                <p>Cost Per Unit: $100,000</p>
+                <p>Cap Rate: 6%</p>
+                <p>Cash on Cash Return: 4%</p>
+                <p>Debt Service Coverage Ratio: 1.2</p>
+              </div>
+            </Col>
+            <Col xs="12" md="6">
+              <div className="summary-section-item">
+                <h4>1 Year Summary</h4>
+                <p>Appreciation</p>
+                <p>Depreciation</p>
+                <p>Amortization</p>
+                <p>Unleveraged IRR: 10%</p>
+                <p>Leveraged IRR: 11%</p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
       <div className="summary-section">
         <div>
           <h1>Investment Health Check</h1>
         </div>
-        <div>
-          <p>Total Expenses %: 40</p>
-          <p>Annual Repairs: $100,000</p>
-          <p>Cap Rate: 6%</p>
-          <p>Vacancy Loss: 4%</p>
-          <p>Insurance: 1.2</p>
-          <p>Kitchen/Bath Rehabs</p>
-        </div>
+        <Container>
+          <Row>
+            <Col xs="12" md="6">
+              <p>Total Expenses %: 40</p>
+              <p>Annual Repairs: $100,000</p>
+              <p>Cap Rate: 6%</p>
+            </Col>
+            <Col xs="12" md="6">
+              <p>Vacancy Loss: 4%</p>
+              <p>Insurance: 1.2</p>
+              <p>Kitchen/Bath Rehabs</p>
+            </Col>
+          </Row>
+        </Container>
       </div>
       <div className="summary-section">
         <h1>Typical Credit Requests at Closing</h1>
